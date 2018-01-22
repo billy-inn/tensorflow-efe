c85d404aa53f41f97fcefe8505df44226f8d9f71
bly <bly930725@gmail.com>
2018-01-21 19:01:30 -0700

    add class DataSet & rewrite config and preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,116 +1,42 @@
 import pandas as pd
-import numpy as np
-import scipy.sparse as sp
 from optparse import OptionParser
 import config
-import gensim
+from utils import data_utils
 
 def preprocess(data_name):
-    if data_name == "wn18":
-        raw_train_file = config.WN18_TRAIN_RAW
-        raw_valid_file = config.WN18_VALID_RAW
-        raw_test_file = config.WN18_TEST_RAW
-        train_file = config.WN18_TRAIN
-        valid_file = config.WN18_VALID
-        test_file = config.WN18_TEST
-        e2id_file = config.WN18_E2ID
-        r2id_file = config.WN18_R2ID
-        sub_mat_file = config.WN18_SUB_MAT
-        obj_mat_file = config.WN18_OBJ_MAT
-    elif data_name == "wn18rr":
-        raw_train_file = config.WN18RR_TRAIN_RAW
-        raw_valid_file = config.WN18RR_VALID_RAW
-        raw_test_file = config.WN18RR_TEST_RAW
-        train_file = config.WN18RR_TRAIN
-        valid_file = config.WN18RR_VALID
-        test_file = config.WN18RR_TEST
-        e2id_file = config.WN18RR_E2ID
-        r2id_file = config.WN18RR_R2ID
-        sub_mat_file = config.WN18RR_SUB_MAT
-        obj_mat_file = config.WN18RR_OBJ_MAT
-    elif data_name == "fb15k":
-        raw_train_file = config.FB15K_TRAIN_RAW
-        raw_valid_file = config.FB15K_VALID_RAW
-        raw_test_file = config.FB15K_TEST_RAW
-        train_file = config.FB15K_TRAIN
-        valid_file = config.FB15K_VALID
-        test_file = config.FB15K_TEST
-        e2id_file = config.FB15K_E2ID
-        r2id_file = config.FB15K_R2ID
-        sub_mat_file = config.FB15K_SUB_MAT
-        obj_mat_file = config.FB15K_OBJ_MAT
-        feat_file = config.FB15K_FEAT
-        model = gensim.models.KeyedVectors.load_word2vec_format(config.FB_EMBEDDING, binary=True)
-    elif data_name == "fb15k237":
-        raw_train_file = config.FB15K237_TRAIN_RAW
-        raw_valid_file = config.FB15K237_VALID_RAW
-        raw_test_file = config.FB15K237_TEST_RAW
-        train_file = config.FB15K237_TRAIN
-        valid_file = config.FB15K237_VALID
-        test_file = config.FB15K237_TEST
-        e2id_file = config.FB15K237_E2ID
-        r2id_file = config.FB15K237_R2ID
-        sub_mat_file = config.FB15K237_SUB_MAT
-        obj_mat_file = config.FB15K237_OBJ_MAT
-        feat_file = config.FB15K237_FEAT
-        model = gensim.models.KeyedVectors.load_word2vec_format(config.FB_EMBEDDING, binary=True)
-    elif data_name == "fb1m":
-        raw_train_file = config.FB1M_TRAIN_RAW
-        raw_valid_file = config.FB1M_VALID_RAW
-        raw_test_file = config.FB1M_TEST_RAW
-        train_file = config.FB1M_TRAIN
-        valid_file = config.FB1M_VALID
-        test_file = config.FB1M_TEST
-        e2id_file = config.FB1M_E2ID
-        r2id_file = config.FB1M_R2ID
+    dataset = data_utils.DataSet(config.DATASET[data_name])
 
-    df_train = pd.read_csv(raw_train_file, sep="\t", names=["e1", "r", "e2"])
-    df_valid = pd.read_csv(raw_valid_file, sep="\t", names=["e1", "r", "e2"])
-    df_test = pd.read_csv(raw_test_file, sep="\t", names=["e1", "r", "e2"])
+    df_train, df_valid, df_test = dataset.load_raw_data()
     df_all = pd.concat([df_train, df_valid, df_test], ignore_index=True)
     train_size = df_train.shape[0]
-    valid_size = df_valid.shape[0]
     test_size = df_test.shape[0]
 
-    outfile = open(e2id_file, "w")
-    e2id = {}
-    for idx, e in enumerate(set(list(df_all.e1) + list(df_all.e2))):
-        e2id[e] = idx
-        outfile.write("%s %d\n" % (e, idx))
-    outfile.close()
-
-    outfile = open(r2id_file, "w")
-    r2id = {}
-    for idx, r in enumerate(set(list(df_all.r))):
-        r2id[r] = idx
-        outfile.write("%s %d\n" % (r, idx))
-    outfile.close()
+    e2id = dataset.save_e2id(set(list(df_all.e1) + list(df_all.e2)))
+    r2id = dataset.save_r2id(set(list(df_all.r)))
 
     df_all.e1 = df_all.e1.map(e2id)
     df_all.e2 = df_all.e2.map(e2id)
     df_all.r = df_all.r.map(r2id)
 
-    data = np.ones(train_size)
-    row = df_all.r[:train_size]
-    col1 = df_all.e1[:train_size]
-    col2 = df_all.e2[:train_size]
-    sub_mat = sp.coo_matrix((data, (row, col1)), shape=(len(r2id), len(e2id)))
-    obj_mat = sp.coo_matrix((data, (row, col2)), shape=(len(r2id), len(e2id)))
-    sub_mat = sub_mat.todok().tocoo()
-    obj_mat = obj_mat.todok().tocoo()
+    # data = np.ones(train_size)
+    # row = df_all.r[:train_size]
+    # col1 = df_all.e1[:train_size]
+    # col2 = df_all.e2[:train_size]
+    # sub_mat = sp.coo_matrix((data, (row, col1)), shape=(len(r2id), len(e2id)))
+    # obj_mat = sp.coo_matrix((data, (row, col2)), shape=(len(r2id), len(e2id)))
+    # sub_mat = sub_mat.todok().tocoo()
+    # obj_mat = obj_mat.todok().tocoo()
 
-    embedding_mat = np.random.uniform(-1, 1, (len(e2id), model.syn0.shape[1]))
-    for e in e2id:
-        if e in model:
-            embedding_mat[e2id[e]] = model[e]
-    feat_mat = np.hstack((sub_mat.todense().T, obj_mat.todense().T, embedding_mat))
+    # embedding_mat = np.random.uniform(-1, 1, (len(e2id), model.syn0.shape[1]))
+    # for e in e2id:
+    #    if e in model:
+    #        embedding_mat[e2id[e]] = model[e]
+    # feat_mat = np.hstack((sub_mat.todense().T, obj_mat.todense().T, embedding_mat))
 
-    df_all[:train_size].to_csv(train_file, header=False, index=False)
-    df_all[train_size:train_size + valid_size].to_csv(valid_file, header=False, index=False)
-    df_all[train_size + valid_size:].to_csv(test_file, header=False, index=False)
-    sp.save_npz(sub_mat_file, sub_mat)
-    sp.save_npz(obj_mat_file, obj_mat)
-    np.save(feat_file, feat_mat)
+    df_train = df_all[:train_size]
+    df_valid = df_all[train_size:-test_size]
+    df_test = df_all[-test_size:]
+    dataset.save_data(df_train, df_valid, df_test)
 
 def parse_args(parser):
     parser.add_option("-d", "--data", type="string", dest="data_name", default="wn18")
